a6a492d5cd86b9ea3001178fb8bd52b49482636c
Yevgeniya <yevgeniya-an@mail.ru>
2020-10-20 20:53:57 +0600

    feat: implement task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
-module.exports = function toReadable (number) {
-  
-}
+const NUMBERS = {
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+    10: 'ten',
+    11: 'eleven',
+    12: 'twelve',
+    13: 'thirteen',
+    14: 'fourteen',
+    15: 'fifteen',
+    16: 'sixteen',
+    17: 'seventeen',
+    18: 'eighteen',
+    19: 'nineteen',
+    20: 'twenty',
+    30: 'thirty',
+    40: 'forty',
+    50: 'fifty',
+    60: 'sixty',
+    70: 'seventy',
+    80: 'eighty',
+    90: 'ninety',
+};
+
+module.exports = function toReadable(number) {
+    if (number === 0) {
+        return 'zero';
+    }
+    let res = [];
+    const [unit, decade, hundreds] = ('' + number).split('').reverse();
+    if (hundreds) {
+        res.push(`${NUMBERS[hundreds]} hundred`);
+    }
+    if (NUMBERS[decade + unit]) {
+        res.push(NUMBERS[decade + unit]);
+    } else {
+        res.push(NUMBERS[decade + '0']);
+        res.push(NUMBERS[unit]);
+    }
+    return res.filter(Boolean).join(' ');
+};
